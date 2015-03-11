d400c0f327f2f7b75e71c2a9b6d90f38559caade
richardf <richardfuch@gmail.com>
2015-03-10 21:48:31 -0300

    partial de notificacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ui.min
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -2,5 +2,9 @@ class PrincipalController < ApplicationController
 
   def index
   end
-  
+
+
+  def create
+    respond_to { |format| format.js }
+  end
 end
